652ea2df922852d8c1f19e8793dd3cccffa98a8e
NikoGB <114112855+NikoGB@users.noreply.github.com>
2023-01-02 04:48:58 -0300

    Add files via upload

--- BACKEND/routes/scheduleRoutes.js
+++ BACKEND/routes/scheduleRoutes.js
@@ -3,9 +3,10 @@ const scheduleController = require('../controllers/scheduleController');
 const api = express.Router();
 
 api.post('/schedule/:id', scheduleController.createSchedule);
-api.get('/schedules', scheduleController.getSchedules);
+api.get('/schedules/:id', scheduleController.getSchedules);
 api.put('/schedule/update/:id', scheduleController.updateSchedule);
 api.delete('/schedule/delete/:id', scheduleController.deleteSchedule);
 api.get('/schedule/search/:id', scheduleController.getSchedule);
+api.get('/schedule/getDayMatrix/:date', scheduleController.getDayMatrixSchedules);
 
 module.exports = api
